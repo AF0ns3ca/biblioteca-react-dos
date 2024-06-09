e74399d53cec836238b1d466eef085b531b80531
AF0ns3ca <afonsecahdez@gmail.com>
2024-06-09 18:43:46 +0200

    último build

--- public/build/assets/Delete-BCskkT3m.js
+++
@@ -1,1 +0,0 @@
-import{a as r,i as t}from"./Footer-DreYb8S2.js";import{j as a}from"./app-BYI2I_UO.js";var e={},o=t;Object.defineProperty(e,"__esModule",{value:!0});var u=e.default=void 0,i=o(r()),l=a;u=e.default=(0,i.default)((0,l.jsx)("path",{d:"M6 19c0 1.1.9 2 2 2h8c1.1 0 2-.9 2-2V7H6zM19 4h-3.5l-1-1h-5l-1 1H5v2h14z"}),"Delete");export{u as d};
